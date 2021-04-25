9649436dcb18a4a3e3371d6228d048230eebbd87
wanglekai <xiaoguailele@foxmail.com>
2021-04-25 19:58:05 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    repo: 'wanglekai/blog',
+    repoLabel: 'GitHub',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLinks: true,
+    editLinkText: '帮助我们改善此页面！'
+  }
+}
